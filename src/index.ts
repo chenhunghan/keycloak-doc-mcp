6ae2aae3e5cb34d036d5df0d36dcdc02c8391598
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2025-04-15 10:48:13 +0300

    chore: make docs an array
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,22 @@ const server = new McpServer({
   version: "0.0.1",
 });
 
-server.resource("readme", "docs://readme", async (uri) => {
-  const res = await fetch(
-    "https://raw.githubusercontent.com/keycloak/keycloak/refs/heads/main/README.md",
-  );
-  const text = await res.text();
-  return { contents: [{ uri: uri.href, text }] };
-});
+const docs = [
+  {
+    resourceName: "readme",
+    resourceUri: "docs://readme",
+    resourceUrl:
+      "https://raw.githubusercontent.com/keycloak/keycloak/refs/heads/main/README.md",
+  },
+];
+
+for (const doc of docs) {
+  server.resource(doc.resourceName, doc.resourceUri, async (uri) => {
+    const res = await fetch(doc.resourceUrl);
+    const text = await res.text();
+    return { contents: [{ uri: uri.href, text }] };
+  });
+}
 
 const transport = new StdioServerTransport();
 await server.connect(transport);
